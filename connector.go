cbd09db802082cb4d339400c23ab91c7a6b69d90
Michael Lu <lumichael94@ip-172-31-28-240.ec2.internal>
2015-07-07 06:02:14 +0000

    finally got something to work

--- connector.go
+++ connector.go
@@ -3,25 +3,40 @@ package main
 import (
 	"fmt"
 	"github.com/gocql/gocql"
-	"log"
+//	"log"
+	"time"
 )
+
+type Session struct{
+	Session_id 	gocql.UUID
+	Coordinator	string
+	Duration	int
+	//Parameters	gocql.map
+	Request		string
+	Started_at	time.Time
+}
 
 func main() {
 	// connect to the cluster
 	cluster := gocql.NewCluster("172.31.28.240")
 	cluster.Keyspace = "system_traces"
-	session, err := cluster.CreateSession()
+	cluster.Consistency = gocql.Quorum
+	session, err:= cluster.CreateSession()
 	if err != nil {
 		panic(fmt.Sprintf("error creating session: %v", err))
 	}
 	defer session.Close()
+	var id string	
+	session.Query(`SELECT session_id FROM system_traces.sessions;`).Consistency(gocql.One).Scan(&id)
+	fmt.Println("Check this out: ", id)
+	var query_sessions = `SELECT session_id, coordinator, duration, request, started_at FROM system_traces.sessions;`
+	q:= session.Query(query_sessions)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
-	var query_sessions = `SELECT session_id, coordinator, duration, parameters, request, started_at FROM system_traces.sessions;`
-	q, err := session.Query(query_sessions)
-	if err != nil {
-		log.Fatal(err)
-	}
-	q.Scan(&sub.Session_id, &sub.Coordinator, &sub.Duration, &sub.Parameters, &sub.Request, &sub.Started_at)
+	sub := Session{}
+	q.Scan(&sub.Session_id, &sub.Coordinator, &sub.Duration, &sub.Request, &sub.Started_at)
 	fmt.Println("Fetched sessions")
 	fmt.Println(sub)
 }
