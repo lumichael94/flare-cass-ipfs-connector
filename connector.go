e465dddb397da2a1cada33fd077aef7e061f3ab6
Developer <dev@grid.home>
2015-07-06 23:12:55 -0400

    edited go file

--- connector.go
+++ connector.go
@@ -10,23 +10,18 @@ func main() {
 	// connect to the cluster
 	cluster := gocql.NewCluster("172.31.28.240")
 	cluster.Keyspace = "system_traces"
-	cluster.Consistency = gocql.Quorum
-	session, _ := cluster.CreateSession()
+	session, err := cluster.CreateSession()
+	if err != nil {
+		panic(fmt.Sprintf("error creating session: %v", err))
+	}
 	defer session.Close()
 
-	/* Search for a specific set of records whose 'timeline' column matches
-	 * the value 'me'. The secondary index that we created earlier will be
-	 * used for optimizing the search */
-	var query_sessions = "COPY sessions (session_id, coordinator, duration, parameters, request, started_at) TO '~/tracing_session.csv' WITH DELIMITER = '|' AND NULL = '<null>';"
-
-	var query_events = "COPY events (session_id, event_id, activity, source, source_elapsed, thread) TO '~/tracing_events.csv' WITH DELIMITER = '|' AND NULL = '<null>';"
-	if err := session.Query(query_sessions).Consistency(gocql.One); err != nil {
+	var query_sessions = `SELECT session_id, coordinator, duration, parameters, request, started_at FROM system_traces.sessions;`
+	q, err := session.Query(query_sessions)
+	if err != nil {
 		log.Fatal(err)
 	}
+	q.Scan(&sub.Session_id, &sub.Coordinator, &sub.Duration, &sub.Parameters, &sub.Request, &sub.Started_at)
 	fmt.Println("Fetched sessions")
-
-	if err := session.Query(query_events).Consistency(gocql.One); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Fetched events")
+	fmt.Println(sub)
 }
